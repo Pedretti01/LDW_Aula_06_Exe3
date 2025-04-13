7ac912f306a48d002be1e0f7fd8f1d4bd2fd829b
Pedretti01 <raphael.silva130@fatec.sp.gov.br>
2025-04-13 03:14:31 -0300

    first commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from controllers import routes
 from models.database import db
 import os
-import pymysql
 
 
 app = Flask(__name__, static_folder='static', template_folder='views')
@@ -16,26 +15,9 @@ DB_NAME = 'boardgames'
 app.config['DATABASE_NAME'] = DB_NAME
 
 # Passamos o diretório ao SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:admin@localhost/{DB_NAME}'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(dir, 'models/boardgames.sqlite3')
 
 if __name__ == '__main__':
-    # Conecta ao MySQL para criar o banco de dados, se necessário
-    connection = pymysql.connect(host='localhost',
-                                 user='root',
-                                 password='admin',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    try:
-        with connection.cursor() as cursor:
-            # Cria o banco de dados se ele não existir
-            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
-            print(f"O banco de dados está criado!")
-    except Exception as e:
-        print(f"Erro ao criar o banco de dados: {e}")
-    finally:
-        connection.close()
-        
     # Inicializa a aplicação Flask
     db.init_app(app=app)
     with app.test_request_context():
